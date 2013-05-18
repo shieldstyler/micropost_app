47489f0da32e440ff7d9af43ff39a93de797b63e
Tyler Shields <shieldstyler@rocketmail.com>
2013-05-18 00:31:14 -0700

    Finish Static Pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,22 +1,38 @@
 require 'spec_helper'
   describe "Static pages" do
     describe "Home page" do
-      it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
-      page.should have_content('Sample App')
+      it "should have the h1 'Micropost'" do
+        visit '/static_pages/home'
+        page.should have_selector('h1',:text => 'Micropost')
+      end
+      
+      it "should have the title 'Home'" do
+        visit '/static_pages/home'
+        page.should have_selector('title',:text => "Micropost | Home")
       end
     end
-
     describe "Help page" do
-      it "should have the content 'Help'" do
-      visit '/static_pages/help'
-      page.should have_content('Help')
+      it "should have the h1 'Micropost'" do
+        visit '/static_pages/help'
+        page.should have_selector('h1',:text => 'Micropost')
+      end
+      
+      it "should have the title 'Help'" do
+        visit '/static_pages/help'
+        page.should have_selector('title',:text => "Micropost | Help")
       end
     end
     describe "About page" do
-    it "should have the content 'About'" do
-    visit '/static_pages/about'
-    page.should have_content('About')
+      it "should have the h1 'Micropost'" do
+        visit '/static_pages/about'
+        page.should have_selector('h1',:text => 'Micropost')
+      end
+      it "should have the title 'About Us'" do
+        visit '/static_pages/about'
+        page.should have_selector('title',:text => "Micropost | About Us")
+      end
     end
   end
-end
+   
+   
+
