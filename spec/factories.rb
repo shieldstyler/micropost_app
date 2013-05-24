044560f998332d9149ad4593bab94fb7ea9b5325
Tyler Shields <shieldstyler@rocketmail.com>
2013-05-24 14:48:28 -0700

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
 	factory :user do
-		name "Tyler Shields"
-		email "shieldstyler@rocketmail.com"
-		password "Pa$$word"
-		password_confirmation "Pa$$word"
+		sequence(:name) { |n| "Person #{n}" }
+		sequence(:email) { |n| "person_#{n}@example.com"}
+		password "foobar"
+		password_confirmation "foobar"
+		factory :admin do
+			admin true
+		end
 	end
 end
+
--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -29,9 +29,12 @@ subject { page }
 			before { sign_in user }
 			
 			it { should have_selector('title', text: user.name) }
+			
+			it { should have_link('Users', href: users_path) }
 			it { should have_link('Profile', href: user_path(user)) }
 			it { should have_link('Settings', href: edit_user_path(user)) }
 			it { should have_link('Sign out', href: signout_path) }
+		
 			it { should_not have_link('Sign in', href: signin_path) }
 		end
 	
@@ -68,6 +71,11 @@ subject { page }
 					before { put user_path(user) }
 					specify { response.should redirect_to(signin_path) }
 				end
+
+				describe "visiting the user index" do
+					before { visit users_path }
+					it { should have_selector('title', text: 'Sign in') }
+				end
 			end
 		end
 		describe "as wrong user" do
@@ -83,6 +91,18 @@ subject { page }
 			
 			describe "submitting a PUT request to the Users#update action" do
 				before { put user_path(wrong_user) }
+				specify { response.should redirect_to(root_path) }
+			end
+		end
+
+		describe "as non-admin user" do
+			let(:user) {FactoryGirl.create(:user) }
+			let(:non_admin) { FactoryGirl.create(:user) }
+
+			before { sign_in non_admin }
+
+			describe "submitting a DELETE request to the Users#destroy action" do
+				before { delete user_path(user) }
 				specify { response.should redirect_to(root_path) }
 			end
 		end
